8edaa2b3163ece54ee77df6ca731be72907fa355
Kunal Singh <kunalsingh909090@gmail.com>
2024-12-02 12:23:44 -0600

    fixed bugs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Load the dictionary file
 let dictionary = [];
 
-fetch('dictionary.txt')
+fetch('/dictionary.txt')
   .then(response => {
     if (!response.ok) {
       throw new Error(`Failed to load dictionary: ${response.statusText}`);
